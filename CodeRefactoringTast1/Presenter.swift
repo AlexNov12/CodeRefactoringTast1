9992cb3e62da4bd56f9c9f14a694b22e29c8f077
AlexNov12 <nov.alex96@gmail.com>
2024-11-01 06:23:20 +0300

    Third commit

---
+++ CodeRefactoringTast1/Presenter.swift
@@ -0,0 +1,8 @@
+//
+//  Presenter.swift
+//  CodeRefactoringTast1
+//
+//  Created by Александр Новиков on 31.10.2024.
+//
+
+import Foundation
---
+++ CodeRefactoringTast1/Services/CardsService.swift
@@ -0,0 +1,8 @@
+//
+//  CardsService.swift
+//  CodeRefactoringTast1
+//
+//  Created by Александр Новиков on 31.10.2024.
+//
+
+import Foundation
